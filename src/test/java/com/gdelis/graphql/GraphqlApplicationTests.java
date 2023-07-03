b5acaf8b505f7d1aae7ec48f6a5fcfc4cce18caf
George Delis <gdelis1989@gmail.com>
2023-07-04 00:03:52 +0100

    Initial commit

---
+++ src/test/java/com/gdelis/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdelis.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
